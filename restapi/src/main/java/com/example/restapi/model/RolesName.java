0bb42442be393d830a29e44da9454cd15d9977c7
Bedlu Erpassa <bedluerpassa@Bedlus-MBP.home>
2020-10-31 09:28:59 -0400

    added user domains

---
+++ restapi/src/main/java/com/example/restapi/model/RolesName.java
@@ -0,0 +1,7 @@
+package com.example.restapi.model;
+
+public enum  RolesName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
--- restapi/src/main/java/com/example/restapi/model/User.java
+++ restapi/src/main/java/com/example/restapi/model/User.java
@@ -1,12 +1,10 @@
 package com.example.restapi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -18,9 +16,37 @@ public class User extends AuditModel {
     @Size(min=2)
     @NotEmpty(message="a name canot be emptiy")
     private  String name;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=6, max = 100)
+    private String password;
+
     @OneToMany(mappedBy="user")
     private Set <Post> post;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+    public User() {}
+
+    public User(String name, String username, String email, String password) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
     public int getId() {
         return id;
     }
@@ -43,5 +69,37 @@ public class User extends AuditModel {
 
     public void setPosts(Set<Post> posts) {
         this.post = posts;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
